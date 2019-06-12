6a6c7a5190607543f385d87c7564620c52ec31ce
JensWalter <jens@apimeister.com>
2019-06-12 15:05:02 +0000

    initial

---
+++ piechart/index.js
@@ -0,0 +1,24 @@
+const { CanvasRenderService } = require('chartjs-node-canvas');
+const AWS = require("aws-sdk");
+
+exports.handler = async (event, context) => {
+  const width = event.width;
+    const height = event.height;
+    let labels = event.data.map((e)=> e.label );
+    let data = event.data.map((e)=> e.value ); 
+    let backgrounds = event.data.map((e)=> e.color );
+    const configuration = {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: backgrounds
+            }]
+        }
+    };
+    const canvasRenderService = new CanvasRenderService(width, height, (ChartJS) => { });
+    const image = await canvasRenderService.renderToBuffer(configuration);
+
+    return {b64PNG : image.toString('base64')};
+}
